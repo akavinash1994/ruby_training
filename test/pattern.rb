9ed97632364d9a1658970f2a874119516ff1e0fc
Avinash <aksingh@grepruby.com>
2017-07-04 21:09:21 +0530

    Fixed intentation

--- test/pattern.rb
+++ test/pattern.rb
@@ -1,14 +1,13 @@
-require_relative "../code/pattern"
-require "test/unit"
+require_relative '../code/pattern'
+require 'test/unit'
 
 class TestPattern < Test::Unit::TestCase
-
   def test_simple
     expected = "*\n"\
                "**\n"\
                "***\n"\
                "****\n"\
-               "*****"
+               '*****'
     assert_equal(expected, Pattern.new(5).generate_pattern)
   end
 end
