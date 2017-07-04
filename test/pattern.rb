f8ec3bf7c4c6f77630fcd08aca93596cd674e8b2
Avinash <aksingh@grepruby.com>
2017-07-04 20:34:05 +0530

    Implement unit test for right align star pyramid program

---
+++ test/pattern.rb
@@ -0,0 +1,14 @@
+require_relative "../code/pattern"
+require "test/unit"
+
+class TestPattern < Test::Unit::TestCase
+
+  def test_simple
+    expected = "*\n"\
+               "**\n"\
+               "***\n"\
+               "****\n"\
+               "*****"
+    assert_equal(expected, Pattern.new(5).generate_pattern)
+  end
+end
