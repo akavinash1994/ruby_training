0ea7dcd2cc5aaa22bb62ec3ecedaba20d45676db
Avinash <aksingh@grepruby.com>
2017-07-05 10:43:42 +0530

    fixed intentation

--- code/array_frequency.rb
+++ code/array_frequency.rb
@@ -1,14 +1,13 @@
 class ArrayFrequency
   def initialize(arg)
-    @arr= arg      
+    @arr = arg
   end
 
   def count
-    freq = Hash.new {0}
+    freq = Hash.new { 0  }
     @arr.each do |e|
       freq[e] += 1
     end
     freq
   end
 end
-
--- test/array_frequency.rb
+++ test/array_frequency.rb
@@ -1,9 +1,8 @@
-require_relative "../code/array_frequency"
-require "test/unit"
- 
+require_relative '../code/array_frequency'
+require 'test/unit'
 class TestArrayFrequency < Test::Unit::TestCase
   def test_simple
-    expected = {"1" => 1, "2" => 1, "a" => 1, "b" => 1}
-    assert_equal(expected, ArrayFrequency.new(["1", "2", "a", "b"]).count )
+    expected = { '1' => 1, '2' => 1, 'a' => 1, 'b' => 1 }
+    assert_equal(expected, ArrayFrequency.new(['1', '2', 'a', 'b']).count)
   end
 end
