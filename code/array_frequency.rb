55c0e4cfdaf4c9f548f63076ca1c3811f8521f13
Avinash <aksingh@grepruby.com>
2017-07-05 10:28:26 +0530

    Implement  array frequency  program

---
+++ code/array_frequency.rb
@@ -0,0 +1,14 @@
+class ArrayFrequency
+  def initialize(arg)
+    @arr= arg      
+  end
+
+  def count
+    freq = Hash.new {0}
+    @arr.each do |e|
+      freq[e] += 1
+    end
+    freq
+  end
+end
+
