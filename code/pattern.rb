a2918a6cee82a15b5034756b9bfe7fbb9d6db48b
akavinash1994 <aksingh@grepruby.com>
2017-07-07 16:28:45 +0530

    Implement Star Pyramid Program (#1)
    * Implement unit test for right align star pyramid program
    * Implement right align star pyramid program
    * remove unused .gitignore files
    * Fixed intentation

---
+++ code/pattern.rb
@@ -0,0 +1,13 @@
+class Pattern
+  def initialize(height)
+    @height = height
+  end
+
+  def generate_pattern
+    str = ''
+    (1..@height).each do |row|
+      str += ('*' * row) + "\n"
+    end
+    str.strip
+  end
+end
